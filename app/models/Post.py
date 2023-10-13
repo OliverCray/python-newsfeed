d8ebd80b06445c3a13279e12c92142ea01a99394
Oliver Cray <128925359+OliverCray@users.noreply.github.com>
2023-10-13 14:12:41 +0100

    Updated Post model to include vote_count

--- app/models/Post.py
+++ app/models/Post.py
@@ -1,7 +1,9 @@
 from datetime import datetime
 from app.db import Base
-from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
-from sqlalchemy.orm import relationship
+from .Vote import Vote
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, select, func
+from sqlalchemy.orm import relationship, column_property
+from sqlalchemy.ext.hybrid import hybrid_property
 
 class Post(Base):
   __tablename__ = 'posts'
@@ -14,3 +16,18 @@ class Post(Base):
 
   user = relationship('User')
   comments = relationship('Comment', cascade='all,delete')
+  votes = relationship('Vote', cascade='all,delete')
+
+  @hybrid_property
+  def vote_count(self):
+      # Calculate the vote count using Python
+      return sum(vote.value for vote in self.votes)
+
+  @vote_count.expression
+  def vote_count(cls):
+      # Calculate the vote count using SQL
+      return (
+          select([func.sum(Vote.value)])
+          .where(Vote.post_id == cls.id)
+          .label("vote_count")
+      )
